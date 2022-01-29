c718913372684e538c9b49d0d8d3708eef2af5e7
elham923 <elham.abushetayyah@htu.edu.jo>
2022-01-29 13:44:34 +0200

    config.js

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Q5LfCBW1M', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '31c4b09b79ar3ku8mb6tpt5ko9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
