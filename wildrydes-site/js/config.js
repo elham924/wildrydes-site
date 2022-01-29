91c526a80437eca34ce43ad8c9aa4a0aa4f60ea8
elham924 <61681657+elham924@users.noreply.github.com>
2022-01-29 14:39:07 +0200

    Add files via upload

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_63AePOtrP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '14id6lej3up70tbf280rtri2t8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
