e0c0781672dd0640d61121044a6b36ce03003323
elham924 <61681657+elham924@users.noreply.github.com>
2022-01-29 12:59:49 +0200

    Update config.js

--- wildrydes-site/js/config.js
+++ wildrydes-site/js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_Q5LfCBW1M', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '31c4b09b79ar3ku8mb6tpt5ko9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'sa-east-1' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://an2eirbqsk.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
